0d9899bf7ecdd641be9b741791e77cbae6ef63a5
Hassan El Abdallah <hassankh613@gmail.com>
2023-06-02 14:24:34 +0300

    Add reviews controllers

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,12 +1,38 @@
-
+const Review = require("../models/review")
 
 const addReviewController = async (req, res) => {
 
     try{
 
+        const body = req.body
+
+        let review = await Review.findOne({
+            where:{
+                doctor_id: req.params.id,
+                user_id: req.userId
+            }
+        })
+
+        // Check if the user is trying to add more than review for the same doctor
+        if (review){
+            return res.status(400).send({Message:"You cannot add more than one review for the same doctor"})
+        }
+
+        review = await Review.create({
+            doctor_id: req.params.id,
+            user_id: req.userId,
+            rate: body.rate,
+            description: body.description,
+            date_added: new Date()
+        })
+
+        await review.save()
+
+        return res.json(review)
+
     } catch(error) {
         console.log(error);
-        // return error status based on the error catched
+        return res.status(404).send({ Message: "Something went wrong" });
     }
 
 }
@@ -15,9 +41,18 @@ const deleteReviewController = async (req, res) => {
 
     try{
 
+        await Review.destroy({
+            where:{
+                user_id: req.userId,
+                doctor_id: req.params.id
+            }
+        })
+
+        return res.json(true)
+
     } catch(error) {
         console.log(error);
-        // return error status based on the error catched
+        return res.status(404).send({ Message: "Review not found" });
     }
 
 }
@@ -48,9 +83,17 @@ const getDoctorReviewsController = async (req, res) => {
 
     try{
 
+        const reviews = await Review.findAll({
+            where:{
+                doctor_id: req.params.id
+            }
+        })
+
+        return res.json(reviews)
+
     } catch(error) {
         console.log(error);
-        // return error status based on the error catched
+        return res.status(404).send({ Message: "Something went wrong" });
     }
 
 }
